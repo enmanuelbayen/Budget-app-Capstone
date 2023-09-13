61cc6daa63a5e68f49d697595cea477c95003f70
enmanuelbayen <enmanuel0495@gmail.com>
2023-09-13 14:16:13 -0400

    create integration test for group/purchase view - add font family proxima nova and update alert style

--- spec/views/groups/index.html.erb_spec.rb
+++ spec/views/groups/index.html.erb_spec.rb
@@ -1,5 +1,35 @@
-require 'rails_helper'
+require './spec/rails_helper'
 
-RSpec.describe 'groups/index.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'groups/index.html.erb', type: :system, js: true do
+  describe 'Index page' do
+    before(:each) do
+      file = File.open(Rails.root.join('app', 'assets', 'images', 'planetbg.jpg'))
+      @user = User.create(name: 'test', email: 'test@example.com', password: 'password', password_confirmation: 'password')
+      @group = Group.create(name: 'Macdonald', icon: { io: file, filename: 'planetbg.jpg', content_type: 'image/jpeg' },
+      author_id: @user.id)
+      sign_in @user
+      visit groups_path
+    end
+
+    it 'displays the groups' do
+      expect(page).to have_content('Macdonald')
+    end
+
+    it 'links to the group show page' do
+      click_link 'Macdonald'
+      expect(page).to have_content('TRANSACTION')
+    end
+
+    it 'displays the total purchases for each group' do
+      expect(page).to have_content('$0')
+    end
+
+    it 'displays the "NEW CATEGORY" button' do
+      expect(page).to have_link('New Category', href: new_group_path)
+    end
+
+    it 'does not display the splash screen' do
+      expect(page).to_not have_content('Welcome to the app!')
+    end
+  end
 end
--- spec/views/purchases/index.html.erb_spec.rb
+++ spec/views/purchases/index.html.erb_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe 'purchases/index.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'purchases/index.html.erb', type: :system do
+  describe 'index page' do
+    before(:each) do
+      file = File.open(Rails.root.join('app', 'assets', 'images', 'planetbg.jpg'))
+      @user = User.create(name: 'Enma', email: 'test@example.com', password: 'password', password_confirmation: 'password')
+      @group = Group.create(name: 'Macdonald', icon: { io: file, filename: 'planetbg.jpg', content_type: 'image/jpeg' },
+      author_id: @user.id)
+      @purchase = Purchase.create(name: 'burger', amount: 100, author_id: @user.id)
+      @group.purchases << @purchase
+      sign_in @user
+      visit group_purchases_path(@group.id)
+    end
+
+    it 'displays the categories' do
+      expect(page).to have_content('Macdonald')
+    end
+
+    it 'displays the total payment for the group' do
+      expect(page).to have_content('$100.0')
+    end
+    
+    it 'displays the purchases for the group' do
+      expect(page).to have_content('burger')
+      expect(page).to have_content('$100')
+    end
+
+    it 'displays the new category form' do
+      expect(page).to have_link('New Transaction', href: new_group_purchase_path(@group))
+    end
+
+  end
 end
